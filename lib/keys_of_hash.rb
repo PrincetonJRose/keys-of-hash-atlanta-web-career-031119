45fed77c4b0ba5c056467f5263a9ca27826d7744
Princeton Rose <decoyzpr@gmail.com>
2019-03-09 14:43:08 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,8 @@
 require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
-    arguments = *arguments
-    if arguments.include?("Panama")
-      return ["red-footed tortoise"]
-    end
+
     arguments
   end
 end
